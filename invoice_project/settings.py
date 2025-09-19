d7edb7ed880b0339e10a2cb8f99a7203bacfd5d8
kadi-life <zorkpolonglife58@gmail.com>
2025-09-18 14:53:49 -1200

    secret key easy access

--- invoice_project/settings.py
+++ invoice_project/settings.py
@@ -21,8 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# Use environment variable for SECRET_KEY in production
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-)pzbpjk*m#@$=9^izz1bbg8i-&t*ejy4=iwhqs&l)_qpi3r-ny')
+SECRET_KEY = 'django-insecure-)pzbpjk*m#@$=9^izz1bbg8i-&t*ejy4=iwhqs&l)_qpi3r-ny'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
